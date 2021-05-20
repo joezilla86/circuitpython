afcb34e02f4d96fb04bdf0eb8bf9fe2797b72361
Joe Anderson <joezilla86@gmail.com>
2021-05-19 20:38:29 -0700

    Initial commit

---
+++ ports/raspberrypi/boards/arduino_nano_rp2040_connect/mpconfigboard.mk
@@ -0,0 +1,11 @@
+USB_VID = 0x***to be filled in***
+USB_PID = 0x***to be filled in***
+USB_PRODUCT = "Nano RP2040 Connect"
+USB_MANUFACTURER = "Arduino"
+
+CHIP_VARIANT = RP2040
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"  ***no idea what this is***
+
+CIRCUITPY__EVE = 1  ***or this***
